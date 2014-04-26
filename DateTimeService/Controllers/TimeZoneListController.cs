cb0025c0065b3396466d99ca83aa2ac3dbb9e28f
David Breyer <davidbreyer@gmail.com>
2014-04-26 18:39:23 -0400

    Added list of available time zones

---
+++ DateTimeService/Controllers/TimeZoneListController.cs
@@ -0,0 +1,37 @@
+﻿using DateTimeService.Components;
+using DateTimeService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DateTimeService.Controllers
+{
+    public class TimeZoneListController : Controller
+    {
+        //
+        // GET: /TimeZoneList/
+        public ActionResult Index()
+        {
+            var infos = TimeZoneInfo.GetSystemTimeZones();
+
+            var returnValue = new List<TimeZoneModel>();
+
+            foreach (var timeZone in infos)
+            {
+                returnValue.Add(new TimeZoneModel
+                {
+                    Code = TimeZoneHelper.TimeZoneList.TimeZones.SingleOrDefault(x=>x.Value.Equals(timeZone.Id)).Key
+                    ,
+                    Name = timeZone.Id
+                    ,
+                    UtcOffset = timeZone.BaseUtcOffset.ToString()
+                    , IsDaylightSavingsTime = timeZone.IsDaylightSavingTime(DateTime.UtcNow)
+                });
+            }
+
+            return View(returnValue);
+        }
+    }
+}
---
+++ DateTimeService/Models/TimeZoneModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DateTimeService.Models
+{
+    public class TimeZoneModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string UtcOffset { get; set; }
+        public bool IsDaylightSavingsTime { get; set; }
+    }
+}
